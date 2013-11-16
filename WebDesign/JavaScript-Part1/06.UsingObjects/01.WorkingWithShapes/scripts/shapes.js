399b94d4f3c041aad424fda4eb7470443075adaa
niknhattan <nikhattan@yahoo.com>
2013-11-16 16:32:19 +0200

    JS Using Objects

---
+++ WebDesign/JavaScript-Part1/06.UsingObjects/01.WorkingWithShapes/scripts/shapes.js
@@ -0,0 +1,28 @@
+﻿function makePoint(X, Y) {
+    if (typeof X === "number" && typeof Y === "number") {
+        return {
+            X: X,
+            Y: Y,
+        }
+    } else {
+        alert("The coords of the point must be number!");
+    }
+}
+
+function makeLine(point1, point2) {
+    if ((point1.X != undefined) && (point1.Y != undefined) &&
+        (point2.X != undefined) && (point2.Y != undefined)) {
+        return {
+            point1: point1,
+            point2: point2,
+            lineLenght: function () {
+                return Math.sqrt(((point1.X - point2.X) * (point1.X - point2.X)) +
+                    ((point1.Y - point2.Y) * (point1.Y - point2.Y)));
+            }
+        }
+
+
+    } else {
+        alert("The members of line must me valid points!");
+    }
+}
---
+++ WebDesign/JavaScript-Part1/06.UsingObjects/03.DeepCopy/deep-copy.js
@@ -0,0 +1,17 @@
+﻿id = 1
+function deepCopy(inObj) {
+    var cloneObj = new Object;
+    
+    for (var i in inObj) {
+        if (typeof inObj[i] === "object") {
+            id += 1;
+            cloneObj[i] = deepCopy(inObj[i]);
+            id -= 1;
+        }
+        else {
+            cloneObj[i] = inObj[i];
+        }
+    }
+    jsConsole.writeLine(id);
+    return cloneObj;
+}
