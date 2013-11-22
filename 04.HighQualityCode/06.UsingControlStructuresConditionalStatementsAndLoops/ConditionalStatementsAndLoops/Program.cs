b0c1bc85b335fa64df165a43cc7dfd1d895be24a
niknhattan <nikhattan@yahoo.com>
2013-11-22 16:41:23 +0200

    commit

---
+++ 04.HighQualityCode/06.UsingControlStructuresConditionalStatementsAndLoops/ConditionalStatementsAndLoops/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ConditionalStatementsAndLoops
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        public class Chef
+        {
+            private Bowl GetBowl()
+            {
+                //... 
+            }
+            private Carrot GetCarrot()
+            {
+                //...
+            }
+            private void Cut(Vegetable potato)
+            {
+                //...
+            }
+        }
+  
+        public class Bowl
+        {
+        }
+
+        public void Cook()
+        {
+            Potato potato = GetPotato();
+            Carrot carrot = GetCarrot();
+            Bowl bowl;
+            Peel(potato);
+
+            Peel(carrot);
+            bowl = GetBowl();
+            Cut(potato);
+            Cut(carrot);
+            bowl.Add(carrot);
+
+            bowl.Add(potato);
+        }
+
+        private Potato GetPotato()
+        {
+            //...
+        }
+    }
+}
