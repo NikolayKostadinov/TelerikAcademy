fc55ffb490949ed8529cb21a1194c69084fdb439
niknhattan <nikhattan@yahoo.com>
2013-12-02 16:39:29 +0200

    Task 10 (Must be refactored and understood)

---
+++ 05DataStructuresAndAlgorithms/02.LinearDataStructures/10.ShortestSequenceOfOperations/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace ShortestSequenceOfOperations
+{
+    using System;
+    class Node<T>
+    {
+        public T Value { get; set; }
+
+        public Node<T> Previous { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ 05DataStructuresAndAlgorithms/02.LinearDataStructures/10.ShortestSequenceOfOperations/ReversedLinkedList.cs
@@ -0,0 +1,27 @@
+﻿namespace ShortestSequenceOfOperations
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class ReversedLinkedList<T> : IEnumerable<T>
+    {
+        public Node<T> Last { get; set; }
+
+        public ReversedLinkedList(Node<T> last)
+        {
+            this.Last = last;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (Node<T> current = this.Last; current != null; current = current.Previous)
+                yield return current.Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
