bd04bbf3f4f8cac2c8b2a264daf10f262495fed8
niknhattan <nikhattan@yahoo.com>
2013-12-02 08:57:25 +0200

    Some Changes

--- 04.HighQualityCode/12.TestDrivenDevelopment/PokerHandsChecker.cs
+++ 04.HighQualityCode/12.TestDrivenDevelopment/PokerHandsChecker.cs
@@ -51,7 +51,48 @@ namespace Poker
 
         public bool IsHighCard(IHand hand)
         {
-            throw new NotImplementedException();
+            bool moreThanTwoFomTheSameSuit = CheckAllEqualSuits(hand);
+            bool moreThanOneFromTheSameFace = CheckFaceForDublication(hand);
+            return !moreThanOneFromTheSameFace && !moreThanTwoFomTheSameSuit;
+        }
+
+        private bool CheckAllEqualSuits(IHand hand)
+        {
+            bool allEqualSuits = true;
+            ICard previouseCard = hand.Cards[0];
+            int lastIndex = hand.Cards.Count;
+            for (int index = 1; index < lastIndex; index++)
+            {
+                if (previouseCard.Suit != hand.Cards[index].Suit)
+                {
+                    allEqualSuits = false;
+                    break;
+                }
+            }
+
+            return allEqualSuits;
+        }
+
+        private bool CheckFaceForDublication(IHand hand)
+        {
+            bool isThereDublicatedFaces = false;
+            int[] countFaces = new int[(int)CardFace.Ace + 1];
+            int lastIndex = hand.Cards.Count;
+
+            for (int index = 0; index < lastIndex; index++)
+            {
+                countFaces[(int)hand.Cards[index].Face]++;
+            }
+
+            foreach (var counter in countFaces)
+            {
+                if (counter > 1)
+                {
+                    isThereDublicatedFaces = true;
+                    break;
+                }
+            }
+            return isThereDublicatedFaces;
         }
 
         public int CompareHands(IHand firstHand, IHand secondHand)
---
+++ 04.HighQualityCode/12.TestDrivenDevelopment/PokerTests/PokerHandsCheckerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+
+namespace PokerTests
+{
+    [TestClass]
+    public class PokerHandsCheckerTest
+    {
+        [TestMethod]
+        public void IsHighCardTest()
+        {
+            Hand hand = new Hand(new List<ICard>() 
+            { 
+                new Card(CardFace.Ace, CardSuit.Spades),
+                new Card(CardFace.Eight, CardSuit.Clubs),
+                new Card(CardFace.Ten, CardSuit.Diamonds),
+                new Card(CardFace.Jack, CardSuit.Hearts),
+                new Card(CardFace.Two, CardSuit.Spades)
+            });
+
+            PokerHandsChecker checker = new PokerHandsChecker();
+            bool actual = checker.IsHighCard(hand);
+            bool expected = true; 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
