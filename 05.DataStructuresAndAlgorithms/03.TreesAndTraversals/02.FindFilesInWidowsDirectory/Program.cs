5bef9b3dd3ed4dcaf39b932a34988629c7d706b4
niknhattan <nikhattan@yahoo.com>
2013-12-04 16:20:57 +0200

    Windows Directory

---
+++ 05.DataStructuresAndAlgorithms/03.TreesAndTraversals/02.FindFilesInWidowsDirectory/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace FindFilesInWidowsDirectory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+    using Tree;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime begin = DateTime.Now;
+            string directoryName = @"C:\Windows";
+            GetDirectory(directoryName);
+            DateTime end = DateTime.Now;
+            Console.WriteLine(end - begin);
+        }
+
+        private static void GetDirectory(string directory)
+        {
+
+            bool isDirectoryExist = Directory.Exists(directory);
+
+            if (!isDirectoryExist)
+            {
+                throw new ArgumentException("Invalid directoryName!!!");
+            }
+            try
+            {
+                var files = Directory.GetFileSystemEntries(directory, "*.exe");
+
+                foreach (var fileName in files)
+                {
+                    string fileFullPath = fileName;
+                    Console.WriteLine(fileFullPath);
+                }
+
+                var directories = Directory.GetDirectories(directory);
+
+                foreach (var directoryName in directories)
+                {
+                    GetDirectory(directoryName);
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
