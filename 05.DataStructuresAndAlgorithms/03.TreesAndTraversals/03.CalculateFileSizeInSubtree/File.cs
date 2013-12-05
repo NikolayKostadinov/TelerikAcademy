926260b9b6b7e1d9fa1a1463dd1ac86b2dc9f950
niknhattan <nikhattan@yahoo.com>
2013-12-05 10:57:53 +0200

    Homework task 3

---
+++ 05.DataStructuresAndAlgorithms/03.TreesAndTraversals/03.CalculateFileSizeInSubtree/File.cs
@@ -0,0 +1,50 @@
+﻿namespace CalculateFileSizeInSubtree
+{
+    using System;
+    using System.Linq;
+
+    public class File
+    {
+        string name;
+        long sizeBytes;
+
+        public File(string name, long size)
+        {
+            this.Name = name;
+            this.SizeInBytes = size;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (value == null || value == "")
+                {
+                    throw new ArgumentException("File name cannot be null or empty");
+                }
+
+                this.name = value;
+            }
+        }
+        public long SizeInBytes
+        {
+            get
+            {
+                return this.sizeBytes;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Size cannot be less than zero");
+                }
+
+                this.sizeBytes = value;
+            }
+        }
+    }
+}
---
+++ 05.DataStructuresAndAlgorithms/03.TreesAndTraversals/03.CalculateFileSizeInSubtree/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CalculateFileSizeInSubtree
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tree is building, please wait for few seconds...");
+            DirectoryTree dirTree = new DirectoryTree("D:\\");
+
+            Console.WriteLine("Done.");
+
+            Console.WriteLine("\nTree size: ");
+            double sizeInMB = dirTree.CalculateSizeOfTree();
+            Console.WriteLine("{0:F2} megabytes", sizeInMB);
+            Console.WriteLine("{0:F2} or gigabytes", sizeInMB / 1024);
+
+            //in windows 8 AppCompat exists, you can try any other folder in Windows directory
+            Console.WriteLine("\nCalculate size of tree folder and subfolders: ");
+            sizeInMB = dirTree.CalculateSizeOfFolder("Work");
+            Console.WriteLine("{0:F2} megabytes", sizeInMB);
+            Console.WriteLine("{0:F2} or gigabytes", sizeInMB / 1024);
+
+        }
+    }
+}
