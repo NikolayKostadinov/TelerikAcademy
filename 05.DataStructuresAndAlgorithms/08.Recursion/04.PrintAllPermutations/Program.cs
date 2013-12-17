22daf1c80613ccc33e3529a194d0ed48e47e9ecd
niknhattan <nikhattan@yahoo.com>
2013-12-17 11:30:57 +0200

    solved problem 6

--- 05.DataStructuresAndAlgorithms/08.Recursion/04.PrintAllPermutations/Program.cs
+++ 05.DataStructuresAndAlgorithms/08.Recursion/04.PrintAllPermutations/Program.cs
@@ -1,15 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _04.PrintAllPermutations
+﻿namespace PrintAllPermutations
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] inputArr = new int[n];
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                inputArr[i] = i + 1;
+            }
+
+            PrintPermutations(n, inputArr);
+        }
+
+        private static void PrintPermutations(int n, int[] result)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("{" + string.Join(", ", result) + "}");
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int oldValue = result[i];
+                result[i] = result[n-1];
+                result[n - 1] = oldValue;
+                PrintPermutations(n - 1, result);
+                oldValue = result[i];
+                result[i] = result[n - 1];
+                result[n - 1] = oldValue;
+            }
         }
     }
 }
---
+++ 05.DataStructuresAndAlgorithms/08.Recursion/PrintingAllVariationsKelementFromNelements/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PrintingAllVariationsKelementFromNelements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int k = 2;
+            int[] set1 = { 1, 2, 3 }; 
+            string[] set = { "hi", "a", "b" };
+
+            PrintAllVastiations<string>(0, set, new string[k]);
+            Console.Write("\b\b ");
+            Console.WriteLine();
+        }
+
+        private static void PrintAllVastiations<T>(int currentIndex, T[] set, T[] result)
+        {
+            if (currentIndex == result.Length)
+            {
+                Console.Write("(" + string.Join(" ", result) + "), ");
+                return;
+            }
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                result[currentIndex] = set[i];
+                PrintAllVastiations<T>(currentIndex + 1, set, result);
+            }
+        }
+    }
+}
