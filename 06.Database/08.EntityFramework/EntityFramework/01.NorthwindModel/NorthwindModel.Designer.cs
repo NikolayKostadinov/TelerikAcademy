e07bab1de85113776ca82bf038fd47d91beac004
niknhattan <nikhattan@yahoo.com>
2014-01-11 09:28:07 +0200

    xml .NET Parsers

--- 06.Database/08.EntityFramework/EntityFramework/01.NorthwindModel/NorthwindModel.Designer.cs
+++ 06.Database/08.EntityFramework/EntityFramework/01.NorthwindModel/NorthwindModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\TelerikAcademyHomeworks\Homeworks\06.Database\08.EntityFramework\EntityFramework\01.NorthwindModel\NorthwindModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\TelericAcademy\Homeworks\06.Database\08.EntityFramework\EntityFramework\01.NorthwindModel\NorthwindModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ 06.Database/17.XMLProcessingIn.NET/XMLProcessing/DOMextractor/DomExtractor.cs
@@ -0,0 +1,43 @@
+﻿/* Write program that extracts all different artists which are found in the catalog.xml. 
+ * For each author you should print the number of albums in the catalogue. 
+ * Use the DOM parser and a hash-table.
+ */
+namespace DOMextractor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    class DomExtractor
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> countOfAlbumsByArtists = new Dictionary<string, int>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../../catalog.xml");
+            Console.WriteLine("Loaded XML document:");
+            Console.WriteLine();
+
+            XmlNode rootNode = doc.DocumentElement;
+
+            foreach (XmlNode node in rootNode.ChildNodes)
+            {
+                string key = node["artist"].InnerText;
+
+                if (countOfAlbumsByArtists.ContainsKey(key))
+                {
+                    countOfAlbumsByArtists[key]++;
+                }
+                else
+                {
+                    countOfAlbumsByArtists.Add(key, 1);
+                }
+            }
+
+            foreach (var artist in countOfAlbumsByArtists)
+            {
+                Console.WriteLine("Artist: {0} has {1} albums incatalog.", artist.Key, artist.Value);
+            }
+        }
+    }
+}
