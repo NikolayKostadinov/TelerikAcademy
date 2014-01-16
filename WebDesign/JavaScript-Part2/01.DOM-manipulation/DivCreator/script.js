c975a9487c5fd175c45146747061074585aa7045
niknhattan <nikhattan@yahoo.com>
2014-01-16 16:32:42 +0200

    JS2

---
+++ WebDesign/JavaScript-Part2/01.DOM-manipulation/DivCreator/script.js
@@ -0,0 +1,72 @@
+﻿//Random width and height between 20px and 100px
+//Random background color
+//Random font color
+//Random position on the screen (position:absolute)
+//A strong element with text "div" inside the div
+//Random border radius
+//Random border color
+//Random border width between 1px and 20px
+function LoadDivs() {
+    var docFragment = document.createDocumentFragment();
+    docFragment.appendChild(GenerateDivs(100));
+    document.body.appendChild(docFragment);
+}
+
+function GenerateDivs(numberOfDivs) {
+    var docFragment = document.createDocumentFragment();
+    for (var i = 0; i < numberOfDivs; i++) {
+        var width = GetRandomBetween(1, 300);
+        var height = GetRandomBetween(1, 300);
+        var backgroundColor = GetColor();
+        var fontColor = GetColor();
+        var position = GetCoord(height, width);
+        var borderRadius = GetRandomBetween(0, Math.min(height, width));
+        var borderColor = GetColor();
+        var borderWidth = GetRandomBetween(1, 20)
+        var div = ConstructDiv(width, height, backgroundColor, fontColor, position, borderRadius, borderColor, borderWidth);
+        var strong = document.createElement("strong");
+        strong.innerHTML = "div";
+        div.appendChild(strong.cloneNode(true));
+        docFragment.appendChild(div.cloneNode(true));
+    }
+
+    return docFragment;
+}
+
+function GetRandomBetween(min, max) {
+    var number = (min + Math.random() * (max - min)) | 0;
+    return number;
+}
+
+function GetColor() {
+    var red = ((Math.random() * 256) | 0) + ', ';
+    var green = ((Math.random() * 256) | 0) + ', ';
+    var blue = ((Math.random() * 256) | 0) + ')';
+    return 'rgb(' + red + green + blue;
+}
+
+function GetCoord(height, width) {
+    var coords =
+        {
+            left: (1 + Math.random() * 1750 - width) | 0,
+            top: (1 + Math.random() * 910 - height) | 0
+        };
+    return coords;
+}
+
+function ConstructDiv(width, height, backgroundColor, fontColor, position, borderRadius, borderColor, borderWidth) {
+    var div = document.createElement("div");
+    div.style.position = 'absolute';
+    div.style.width = width + 'px';
+    div.style.backgroundColor = backgroundColor;
+    div.style.color = fontColor;
+    div.style.height = height + 'px';
+    div.style.top = position.top + 'px';
+    div.style.left = position.left + 'px';
+    div.style.borderRadius = borderRadius + 'px';
+    div.style.borderWidth = borderWidth + 'px';
+    div.style.borderStyle = 'solid';
+    div.style.borderColor = borderColor;
+    return div;
+  
+}
---
+++ WebDesign/JavaScript-Part2/01.DOM-manipulation/DivsMovingInCircle/script.js
@@ -0,0 +1,54 @@
+﻿(function () {
+    'use strict';
+
+    var count = 12;
+    var radius = 50;
+    var divs = Array(count);
+    var angles = Array(count);
+
+    //function GenerateDivs() {
+        var div = document.createElement("div");
+        div.style.position = 'absolute';
+        div.style.height = '20px';
+        div.style.width = '20px';
+        div.style.border = '2px solid #0094ff';
+        div.style.backgroundColor = '#0ff'
+        div.style.borderRadius = '20px';
+
+        for (var i = 0; i < divs.length; i++) {
+
+            divs[i] = CalculatePosition(0, 360 / count, div.cloneNode(true), radius);
+            angles[i] = i * 360 / count;
+        }
+
+        for (var i = 0; i < divs.length; i++) {
+            document.body.appendChild(divs[i]);
+        }
+
+        timer = setInterval(function () {
+            var step = -5;
+            for (var i = 0, len = divs.length; i < len; i++) {
+                div[i] = CalculatePosition(angles[i], step, divs[i], radius);
+                angles[i] += step;
+                if (angles[i] > 360) {
+                    angles[i] -= 360;
+                }
+            }
+        }, 100);
+
+    //}
+
+    function CalculatePosition(oldAngle, newAngle, div, radius) {
+        var x = 850;
+        var y = 400;
+
+        var angle = oldAngle + newAngle;
+        if (angle > 360) { angle -= 360; }
+        var cx = x - radius * Math.cos(angle * Math.PI / 180);
+        var cy = y + radius * Math.sin(angle * Math.PI / 180);
+        div.style.top = cy + 'px';
+        div.style.left = cx + 'px';
+
+        return div;
+    }
+})();
