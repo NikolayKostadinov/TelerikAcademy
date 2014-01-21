787d0c21fdf12d33c82f176a66688a76aa1c45ed
niknhattan <nikhattan@yahoo.com>
2014-01-21 19:28:09 +0200

    coloring text

---
+++ WebDesign/JavaScript-Part2/01.DOM-manipulation/ColoringTextArea/script.js
@@ -0,0 +1,7 @@
+﻿//Create a text area and two inputs with type="color"
+//Make the font color of the text area as the value of the first color input
+//Make the background color of the text area as the value of the second input
+(function () {
+    'use strict';
+
+})
