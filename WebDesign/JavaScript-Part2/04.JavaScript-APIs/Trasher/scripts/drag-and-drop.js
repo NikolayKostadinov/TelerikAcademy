02b419b0b6acf5aade773dc411aedc7d5b9c194c
niknhattan <nikhattan@yahoo.com>
2014-01-23 16:50:24 +0200

    Trasher.rar

---
+++ WebDesign/JavaScript-Part2/04.JavaScript-APIs/Trasher/scripts/drag-and-drop.js
@@ -0,0 +1,16 @@
+﻿
+function drag(ev) {
+    ev.dataTransfer.setData('dragged-id', ev.target.id);
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+    ev.target.style.backgroundImage = "url('images/trashcan-open.png')";
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var element = document.getElementById(ev.dataTransfer.getData('dragged-id'));
+    element.parentElement.removeChild(element);
+    ev.target.style.backgroundImage = "url('images/trashcan-close.png')";
+}
---
+++ WebDesign/JavaScript-Part2/04.JavaScript-APIs/Trasher/scripts/multiply-trash.js
@@ -0,0 +1,24 @@
+﻿(function(){
+    function GetCoord(height, width) {
+        var coords =
+            {
+                left: (1 + Math.random() * 1750 - width) | 0,
+                top: (1 + Math.random() * 910 - height) | 0
+            };
+        return coords;
+    }
+
+    (function GenerateTwentyTrashes() {
+        var trashDiv = document.getElementById('trash0');
+        var fragment = document.createDocumentFragment();
+        for (var i = 1; i < 20; i++) {
+            var coords = GetCoord(trashDiv.style.height, trashDiv.style.width);
+            trashDiv.style.position = 'absolute';
+            trashDiv.style.top = coords.top + 'px';
+            trashDiv.style.left = coords.left + 'px';
+            trashDiv.id = 'trash' + i;
+            fragment.appendChild(trashDiv.cloneNode(true));
+        }
+        document.body.appendChild(fragment);
+    })();
+})();
