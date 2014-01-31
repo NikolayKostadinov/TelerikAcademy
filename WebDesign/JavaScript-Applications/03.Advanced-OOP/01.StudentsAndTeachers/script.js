a0d886c5f698576e4a247bb61916cd3630634364
niknhattan <nikhattan@yahoo.com>
2014-01-31 22:08:56 +0200

    Advanced OOP

--- WebDesign/JavaScript-Applications/03.Advanced-OOP/01.StudentsAndTeachers/script.js
+++ WebDesign/JavaScript-Applications/03.Advanced-OOP/01.StudentsAndTeachers/script.js
@@ -6,7 +6,7 @@
     },
 
     introduce: function () {
-        var strRepresentation =  "Name: " + this._firstName + " " + this._lastName + ", Age: " + this._age;
+        var strRepresentation = "Name: " + this._firstName + " " + this._lastName + ", Age: " + this._age;
 
         return strRepresentation;
     }
@@ -14,13 +14,13 @@
 
 var Student = Person.extend({
     init: function (firstName, lastName, age, grade) {
-        this._super = Object.create(this._super); 
+        this._super = Object.create(this._super);
         this._super.init(firstName, lastName, age);
         this._grade = grade;
     },
 
     introduce: function () {
-        var strRepresentation = this._super.introduce()  +", grade: " + this._grade;
+        var strRepresentation = this._super.introduce() + ", grade: " + this._grade;
         return strRepresentation;
     }
 });
@@ -38,34 +38,74 @@ var Teacher = Person.extend({
 });
 
 var Class = {
-    init: function (capacity, form_teacher) {
+    init: function (capacity, formTeacher) {
         this._capacity = capacity;
         this._formTeacher = formTeacher;
-        this._students = new [capacity];
+        this._students = [];
     },
 
     appendStudent: function (student) {
 
+        if (this._students.length == this._capacity) {
+            throw new Error("Capacity of class is exided!");
+        }
+
+        this._students.push(student);
     },
+
+    toString: function () {
+        var strRepresentation = "The former teacher of this class is: " + this._formTeacher.introduce() + '\n';
+        for (var i = 0; i < this._students.length; i++) {
+            if (this._students[i].introduce) {
+                strRepresentation += this._students[i].introduce() + "\n";
+            }
+        }
+        return strRepresentation;
+    }
 }
 
 var School = {
     init: function (name, town) {
         this._name = name;
         this._town = town;
-        this.classes = new [];
+        this._classes = [];
     },
+    appendClass: function (aClass) {
+        this._classes.push(aClass);
+    }
 }
 
-var test = Object.create(Person);
-test.init("Pesho", "Peshev");
-console.log(test.introduce());
 
 var manhattan = Object.create(Student);
 manhattan.init("Nikolay", "Kostadinov", 38, "Ninja");
+
 var drugarkata = Object.create(Teacher);
-drugarkata.init("Майчето", "Манолова", 45, "Изрoд");
+drugarkata.init("Майчето", "Манолова", 45, "Даскалица по простотия");
 
 console.log(manhattan.introduce());
 console.log(drugarkata.introduce());
 
+var myClass = Object.create(Class);
+myClass.init(34, drugarkata);
+myClass.appendStudent(manhattan);
+
+var student = {};
+
+for (var i = 0; i < 34; i++) {
+
+    student = Object.create(Student);
+    student.init("FirstName" + (i + 1), "LastName" + (i + 1), i + 7, 3);
+    try {
+        myClass.appendStudent(student);
+    } catch (e) {
+        console.log(e.message);
+        break;
+    }
+}
+
+var school = Object.create(School);
+school.init("TelerikAcademy", "Sofia");
+school.appendClass(myClass);
+
+console.log(myClass.toString());
+console.log(school);
