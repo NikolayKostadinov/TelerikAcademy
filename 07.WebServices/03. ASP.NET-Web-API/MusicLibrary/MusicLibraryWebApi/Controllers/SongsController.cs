4e46a9ccb27b05479619484559bb655dfb4bea39
niknhattan <nikhattan@yahoo.com>
2014-02-19 14:31:24 +0200

    All

---
+++ 07.WebServices/03. ASP.NET-Web-API/MusicLibrary/MusicLibraryWebApi/Controllers/SongsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MusicLibrary.Models;
+using MusicLibrary.Data;
+
+namespace MusicLibraryWebApi.Controllers
+{
+    public class SongsController : ApiController
+    {
+        private MusicLibraryContext db = new MusicLibraryContext();
+
+        // GET api/Default1
+        public IEnumerable<Song> GetSongs()
+        {
+            var songs = db.Songs.Include(s => s.Artist).Include(s => s.Album);
+            return songs.AsEnumerable();
+        }
+
+        // GET api/Songs/5
+        public Song GetSong(int id)
+        {
+            Song song = db.Songs.Find(id);
+            if (song == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return song;
+        }
+
+        // PUT api/Songs/5
+        public HttpResponseMessage PutSong(int id, Song song)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != song.SongId)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(song).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Songs
+        public HttpResponseMessage PostSong(Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                var newArtist = (from artist in db.Artists
+                                 where artist.ArtistId == song.ArtistId
+                                 select artist).FirstOrDefault();
+                song.Artist = newArtist;
+
+                var newAlbum = (from album in db.Albums
+                                where album.AlbumId == song.AlbumId
+                                select album).FirstOrDefault();
+                song.Album = newAlbum;
+
+                db.Songs.Add(song);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, song);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = song.SongId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Songs/5
+        public HttpResponseMessage DeleteSong(int id)
+        {
+            Song song = db.Songs.Find(id);
+            if (song == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Songs.Remove(song);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, song);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
