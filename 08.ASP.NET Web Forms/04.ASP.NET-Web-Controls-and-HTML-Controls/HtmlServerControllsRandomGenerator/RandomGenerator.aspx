f83a3309eac88b923d986b06ece43f456b2b9c05
niknhattan <nikhattan@yahoo.com>
2014-04-04 16:49:27 +0300

    Homeworks all

---
+++ 08.ASP.NET Web Forms/04.ASP.NET-Web-Controls-and-HTML-Controls/HtmlServerControllsRandomGenerator/RandomGenerator.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RandomGenerator.aspx.cs" Inherits="HtmlServerControllsRandomGenerator.RandomGenerator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title></title>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <div>
+                <label>Generate random number</label>
+                <br />
+                <label for="inputValueFrom">from:</label>
+                <input type="text" id="inputValueFrom" runat="server" />
+                <br />
+                <label for="inputValueTo">to:</label>
+                <input type="text" id="inputValueTo" runat="server" />
+                <br />
+                <button id="Submit" title="Submit" name="Submit" runat="server" onserverclick="Submit_ServerClick">Submit</button>
+                <br />
+                <hr />
+                <label id="Result" runat="server"></label>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ 08.ASP.NET Web Forms/04.ASP.NET-Web-Controls-and-HTML-Controls/HtmlServerControllsRandomGenerator/RandomGenerator.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HtmlServerControllsRandomGenerator
+{
+    public partial class RandomGenerator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Submit_ServerClick(object sender, EventArgs e)
+        {
+            try
+            {
+                var valueFrom = int.Parse(this.inputValueFrom.Value);
+                var valueTo = int.Parse(this.inputValueTo.Value);
+                this.Result.InnerText = GetRandomNumber(valueFrom, valueTo + 1);
+            }
+            catch (Exception ex)
+            {
+                this.Result.InnerText = "This program accepts only integer numeric input!!!";
+            }
+        }
+
+        private string GetRandomNumber(int valueFrom, int valueTo)
+        {
+            var randomGenerator = new Random();
+            var random = randomGenerator.Next(valueFrom, valueTo);
+            return random.ToString();
+        }
+    }
+}
