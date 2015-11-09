db98acf8080fce9535ba719b39b90ec3b1fdfeb8
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2015-11-09 08:28:46 +0200

    Last Changes

--- 09.ASP.NET MVC/BlogArchitectureDemo/BlogSystem.Web.Models/IdentityModels.cs
+++ 09.ASP.NET MVC/BlogArchitectureDemo/BlogSystem.Web.Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace BlogSystem.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("BlogSystemConnection", throwIfV1Schema: false)
         {
         }
 
